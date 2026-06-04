e69b350f169f79871b516efb8357eea6b6650855
Jazzmyn Cruz <graphicperfectity@gmail.com>
2026-06-04 00:04:22 -0400

    Added a menu to initiate the game

--- CoinToss.java
+++ CoinToss.java
@@ -6,51 +6,69 @@ public class CoinToss
     public static void main (String[] args)
     {
 
-        Random random = new Random();
+        System.out.println("Coin Toss Simulator");
+        System.out.println(" ");
+        System.out.println("Menu");
+        System.out.println("1. Toss Coins");
+        System.out.println("2. End Game");
+
+        Scanner menuSc = new Scanner(System.in);
+
+        int choice = menuSc.nextInt();
+
+        if (choice == 1)
+        {
+
+            Random random = new Random();
         
-        int heads = 0;
-        int tails = 0;
+            int heads = 0;
+            int tails = 0;
     
         
-        Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("How many times would you like to toss the coin?");
+            System.out.println("How many times would you like to toss the coin?");
 
-        int tossAmount = sc.nextInt();
+            int tossAmount = sc.nextInt();
 
 
-        for (int i = 0; i < tossAmount; i++)
-        {
-            int toss = random.nextInt(2);
-
-            if (toss == 0)
+            for (int i = 0; i < tossAmount; i++)
             {
-                heads++;   
+                int toss = random.nextInt(2);
 
-            } 
-            else
-            {
-                tails++;
+                if (toss == 0)
+                {
+                   heads++;   
+
+                } 
+                else
+                {
+                    tails++;
                 
+                }
+
             }
 
+
+            System.out.println("Total tosses: " + tossAmount);
+
+            System.out.println("Heads: " + heads);
+        
+            System.out.println("Tails: " + tails);
+
+
         }
-        
-
-        System.out.println("Total tosses: " + tossAmount);
-
-        System.out.println("Heads: " + heads);
-        
-        System.out.println("Tails: " + tails);
-
-
-
-
-
-
-
-
+        else if (choice == 2)
+        {
+          System.out.println("Game Over");
+        }
+        else
+        {
+            System.out.println("Input is not valid.");
+        }
 
     }
+
+        
 
 }
